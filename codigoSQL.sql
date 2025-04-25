56549891a341885d860d16cebb5d6408079d2017
Davi2 <darapac@ulasalle.edu.pe>
2025-04-25 14:32:48 -0500

    Aplicando Normalización

---
+++ codigoSQL.sql
@@ -0,0 +1,93 @@
+CREATE TABLE Cliente (
+  id_cliente INT PRIMARY KEY Identity(1,1),
+  nombre VARCHAR(50),
+  apellido VARCHAR(50),
+  email VARCHAR(100),
+  telefono VARCHAR(20),
+  direccion VARCHAR(100),
+  fecha_registro DATE
+);
+
+CREATE TABLE Marca (
+  id_marca INT PRIMARY KEY Identity(1,1),
+  nombre VARCHAR(50),
+  pais_origen VARCHAR(50)
+);
+
+CREATE TABLE Categoria (
+  id_categoria INT PRIMARY KEY Identity(1,1),
+  nombre_categoria VARCHAR(50)
+);
+
+CREATE TABLE Talla (
+  id_talla INT PRIMARY KEY Identity(1,1),
+  descripcion VARCHAR(10) -- S, M, L, XL, etc.
+);
+
+CREATE TABLE Producto (
+  id_producto INT PRIMARY KEY Identity(1,1),
+  nombre VARCHAR(50),
+  descripcion TEXT,
+  precio DECIMAL(10, 2),
+  stock INT,
+  id_marca INT,
+  id_categoria INT,
+  id_talla INT,
+  FOREIGN KEY (id_marca) REFERENCES Marca(id_marca),
+  FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria),
+  FOREIGN KEY (id_talla) REFERENCES Talla(id_talla)
+);
+
+CREATE TABLE Venta (
+  id_venta INT PRIMARY KEY Identity(1,1),
+  fecha_venta DATE,
+  id_cliente INT,
+  total DECIMAL(10, 2),
+  FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+);
+
+CREATE TABLE DetalleVenta (
+  id_detalleV INT PRIMARY KEY Identity(1,1),
+  id_venta INT,
+  id_producto INT,
+  cantidad INT,
+  precio_unitario DECIMAL(10, 2),
+  subtotal DECIMAL(10, 2),
+  FOREIGN KEY (id_venta) REFERENCES Venta(id_venta),
+  FOREIGN KEY (id_producto) REFERENCES Producto(id_producto)
+);
+
+CREATE TABLE Proveedor (
+  id_proveedor INT PRIMARY KEY Identity(1,1),
+  nombre VARCHAR(50),
+  telefono VARCHAR(20),
+  email VARCHAR(100),
+  direccion VARCHAR(100)
+);
+
+CREATE TABLE Compra (
+  id_compra INT PRIMARY KEY Identity(1,1),
+  id_proveedor INT,
+  fecha_compra DATE,
+  total DECIMAL(10, 2),
+  FOREIGN KEY (id_proveedor) REFERENCES Proveedor(id_proveedor)
+);
+
+CREATE TABLE DetalleCompra (
+  id_detalleC INT PRIMARY KEY Identity(1,1),
+  id_compra INT,
+  id_producto INT,
+  cantidad INT,
+  precio_compra DECIMAL(10, 2),
+  subtotal DECIMAL(10, 2),
+  FOREIGN KEY (id_compra) REFERENCES Compra(id_compra),
+  FOREIGN KEY (id_producto) REFERENCES Producto(id_producto)
+);
+
+CREATE TABLE Empleado (
+  id_empleado INT PRIMARY KEY Identity(1,1),
+  nombre VARCHAR(50),
+  apellido VARCHAR(50),
+  cargo VARCHAR(50),
+  fecha_ingreso DATE
+);
